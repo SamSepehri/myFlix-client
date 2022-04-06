31567c82465c9b26d7aa1b6a4638023dac9dbb1e
samisep <52745072+samisep@users.noreply.github.com>
2022-04-06 18:07:43 +0200

    create and update movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
     render() {
@@ -6,3 +7,13 @@ export class MovieCard extends React.Component {
         return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
     }
 }
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired
+    }).isRequired,
+    index: PropTypes.number.isRequired,
+    onMovieClick: PropTypes.func.isRequired
+};
