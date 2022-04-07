f50962665203b4b69789425a0d3bff2906a3377d
samisep <52745072+samisep@users.noreply.github.com>
2022-04-07 22:13:14 +0200

    Update movie-card.jsx
    Add card, card gtoup and container

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import { CardGroup, Container, Button, Card } from "react-bootstrap";
+
 
 export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
 
         return (
-            <Card>
-                <Card.Img variant="top" src={movie.ImagePath} />
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
-                </Card.Body>
-            </Card>
+            <Container>
+                <CardGroup>
+                    <Card id="movie-card">
+                        <Card.Img variant="top" src={movie.ImagePath} />
+                        <Card.Body>
+                            <Card.Title id="card-title">{movie.Title}</Card.Title>
+                            <Card.Text>{movie.Description}</Card.Text>
+                            <Button id="card-title" onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Container>
         );
     }
 }
