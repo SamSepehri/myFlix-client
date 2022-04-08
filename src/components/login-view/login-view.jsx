1239bc46fae50c66dc649ae116e66f3db2e9cd88
samisep <52745072+samisep@users.noreply.github.com>
2022-04-08 14:59:09 +0200

    Update login-view.jsx
    Add axios.post to login view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -9,7 +10,10 @@ export function LoginView(props) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
+        axios.post('https://cinesam2022.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
         // Send a request to the server for authentication, then call props.onLoggedIn(username)
         props.onLoggedIn(username);
     };
