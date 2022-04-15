83e5854df862c8c9aeed987753be19b32fb9eb8e
samisep <52745072+samisep@users.noreply.github.com>
2022-04-15 11:38:34 +0200

    Add modules
    Add redux developertools
    add redux and react-redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Container from "react-bootstrap/Container";
+import { devToolsEnhancer } from "redux-devtools-extension";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import moviesAPP from "./reducers/reducers";
 import MainView from "./components/main-view/main-view";
 import './index.scss';
+
+const store = createStore(moviesAPP, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <Container id="main-view">
-                <MainView />
-            </Container>
+            <Provider store={store}>
+                <Container id="main-view">
+                    <MainView />
+                </Container>
+            </Provider>
         );
     }
 }
@@ -18,5 +26,5 @@ class MyFlixApplication extends React.Component {
 // Finds the root of the app
 const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React ti render the app in the root DOM element
+// Tells React to render the app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
